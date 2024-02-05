627af2494cfd7d56aef185f2a9a08cb173a959fb
saurabhkrtiwari <saurabhkrtiwari@gmail.com>
2024-02-05 19:49:37 +0530

    jpql multiple column

--- src/main/java/in/saurabhkrtiwari/JPQLExample.java
+++ src/main/java/in/saurabhkrtiwari/JPQLExample.java
@@ -5,12 +5,27 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JPQLExample {
     public static void main(String[] args) {
         //getAllEmployees();
-        getAllEmployeesWithActiveAccessCard();
+       // getAllEmployeesWithActiveAccessCard();
+        getAllEmployeesPaymentsWithName();
+    }
+
+    private static void getAllEmployeesPaymentsWithName() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");
+        EntityManager entityManager =entityManagerFactory.createEntityManager();
+
+        TypedQuery<Object[]> employeeTypedQuery = entityManager.createQuery("select e.id,e.payStubs from Employee e",Object[].class);
+        List<Object[]> resultList = employeeTypedQuery.getResultList();
+        resultList.forEach(s-> System.out.println(s[0]+"---"+s[1]));
+
+        entityManager.close();
+        entityManagerFactory.close();
     }
 
     private static void getAllEmployeesWithActiveAccessCard() {
